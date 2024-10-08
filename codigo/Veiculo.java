3c00d610d820ad35ca2c870970d64879b1b7b836
Arthur Candian <129302284+candian15@users.noreply.github.com>
2024-10-08 09:49:25 -0300

    Add files via upload

---
+++ codigo/Veiculo.java
@@ -0,0 +1,25 @@
+public class Veiculo {
+    private String placa;
+    private String modelo;
+    private String marca;
+    private String dono;
+
+   
+    public String getPlaca() {
+        return placa;
+    }
+
+    
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    
+    public String getDono() {
+        return dono;
+    }
+}
